d725ab4faf5cc0181762a6e294f0a042d104ac14
Zubair Lambat <P14149985@LEC-ADMIN.dmu.ac.uk>
2017-11-30 14:59:05 +0000

    tried changing responsiveness and added a few more content to the website

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,88 +1,23 @@
-$(document).ready(function() {
+(function(){
 
-          /* Sticky Nav bar */
+  let menuButton = document.getElementById("menu");
+  let navMenu = document.getElementById("nav-menu");
 
+  menuButton.addEventListener("click", toggleMenu);
 
-    $('.js--section-features').waypoint(function(direction) {
-        if (direction == "down") {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-    }, {
-      offset: '60px;'
-    });
+  let toggle = false; // hidden at first
+  function toggleMenu(){
+    if (toggle) { // true: it's visible
+      navMenu.classList.remove("show-menu"), // hide it
+      toggle = false
+    }
+    else { // false: it's hidden
+      navMenu.classList.add("show-menu"), // show it
+      toggle = true
+    }
+  }
 
-
-
-        /* Navigation scroll */
-
-          $('.js--scroll-to-plans').click(function () {
-             $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 3000);
-          });
-
-          $('.js--scroll-to-start').click(function () {
-             $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 2000);
-          });
-
-
-          $('.js--how-it-works').click(function () {
-             $('html, body').animate({scrollTop: $('.js--section-works-step').offset().top}, 2000);
-          });-+
-
-          $('.js--location').click(function () {
-             $('html, body').animate({scrollTop: $('.js--located-cities').offset().top}, 2000);
-          });
-
-          $('.js--sign-up').click(function () {
-             $('html, body').animate({scrollTop: $('.js--sign-me-up').offset().top}, 2000);
-          });
-
-          /* Animation scroll */
-
-          $('.js--1').waypoint(function(direction){
-
-                $('.js--1').addClass('animated fadingIn');
-          }, {
-              offset: '50%'
-          });
-
-          $('.js--2').waypoint(function(direction) {
-              $('.js--2').addClass('animated fadingUp');
-          }, {
-              offset: '50%'
-          });
-
-          $('.js--3').waypoint(function(direction) {
-              $('.js--3').addClass('animated fadingIn');
-          }, {
-              offset: '50%'
-          });
-
-          $('.js--4').waypoint(function(direction) {
-              $('.js--4').addClass('animated pulse');
-          }, {
-              offset: '50%'
-          });
-
-          /* Mobile Navigation javascript */
-
-          $('.js--nav-icon').click(function() {
-              var nav = $('.js--main-nav');
-              var icon = $('.js--nav-icon i');
-
-              nav.slideToggle(200);
-
-              if (icon.hasClass('fa-bars')) {
-                  icon.addClass('fa-times');
-                  icon.removeClass('fa-bars');
-              } else {
-                  icon.addClass('fa-bars');
-                  icon.removeClass('fa-times');
-              }
-          });
-
-    });
+})();
 
           function myMap() {
           var mapProp= {
